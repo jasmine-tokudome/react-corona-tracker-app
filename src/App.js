cd67b7e123716f860bb57cf51a02d05df285177a
徳留 美帆 <tokudome@pepabo.com>
2024-02-04 23:44:04 +0900

    TopPageコンテナをApp.jsに読み込ませる

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import TopPage from "./pages/TopPage";
+
+impot TopPahe from './TopPage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>こんばんは</h1>
+      <TopPage />
     </div>
   );
 }
