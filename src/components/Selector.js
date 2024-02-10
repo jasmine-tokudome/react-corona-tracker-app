d3fd98ba1fc1cd7a3c75d0f957c9f3d6252b1cf6
徳留 美帆 <tokudome@pepabo.com>
2024-02-10 23:31:52 +0900

    fetch関数.buttonを設置

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -2,14 +2,21 @@ import {useState} from 'react';
 import countriesJson from '../countries.json';
 
 const Selector = () => {
-  const[country, setCountry] = useState('');
+  const[country, setCountry] = useState("");
+  const getCountryDate =  () => {
+    fetch(`https://monotein-books.vercel.app/api/corona-tracker/country/${country}`)
+      .then(res=>res.json())
+      .then(date=>console.log(date))
+  }
   return(
     <div>
-      <select onChange = {(e)=> setCountry(e.target.value)}>
+      <select onChange={(e)=>setCountry(e.target.value)}>
+        <option>Select A Country</option>
         {countriesJson.map((country,index) =>
-          <option key={index}>{country.Country}</option>
+          <option key={index} value={country.Slug}>{country.Country}</option>
         )}
       </select>
+      <button onClick={getCountryDate}>Get Data</button>
     </div>
   );
 };
