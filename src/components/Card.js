fd9d1d1a2723b1ee745189a43f09aee43eb6050b
徳留 美帆 <tokudome@pepabo.com>
2024-02-23 22:12:23 +0900

    APIから必要なデータを取得

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,13 @@
 const Card = () => {
+  const getAllCountriesData = () => {
+  fetch("https://monotein-books.vercel.app/api/corona-tracker/summary")
+    .then(res => res.json())
+    .then(data => console.log(data.Countries))
+}
   return (
     <div>
       <h2>Card</h2>
+      <button onClick={getAllCountriesData}>Get All Data</button>
     </div>
   );
 };
