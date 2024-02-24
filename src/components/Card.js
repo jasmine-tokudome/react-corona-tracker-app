35ae0974111dc329ca05d923b0752f01250d4df7
徳留 美帆 <tokudome@pepabo.com>
2024-02-24 23:43:25 +0900

    stateを用意

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,20 @@
+import { useState } from "react" ;
+
 const Card = () => {
+  const [allCountriesData, setAllCountriesData] = useState([]); // [
   const getAllCountriesData = () => {
   fetch("https://monotein-books.vercel.app/api/corona-tracker/summary")
     .then(res => res.json())
-    .then(data => console.log(data.Countries))
+    .then(data => setAllCountriesData(data.Countries))
 }
   return (
     <div>
+      {console.log(allCountriesData)}
       <h2>Card</h2>
       <button onClick={getAllCountriesData}>Get All Data</button>
+      {allCountriesData.map((signleData) =>
+      console.log(signleData)
+      )}
     </div>
   );
 };
