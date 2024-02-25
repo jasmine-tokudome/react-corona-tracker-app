763a43d7742c7b4a5a3a6bd2d32712f17a8cddcf
徳留 美帆 <tokudome@pepabo.com>
2024-02-25 23:09:32 +0900

    国別データを出力させる

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,8 +12,12 @@ const Card = () => {
       {console.log(allCountriesData)}
       <h2>Card</h2>
       <button onClick={getAllCountriesData}>Get All Data</button>
-      {allCountriesData.map((signleData) =>
-      console.log(signleData)
+      {allCountriesData.map((singleData,index) =>
+      <div key={index}>
+        <h2>{singleData.Country}</h2>
+        <p>新規感染者：{singleData.NewConfirmed}</p>
+        <p>感染者総数：{singleData.TotalConfirmed}</p>
+      </div>
       )}
     </div>
   );
