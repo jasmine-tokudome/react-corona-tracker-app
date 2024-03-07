43a8ac323d6c4f560ba23aa8357ce5f110cc9597
徳留 美帆 <tokudome@pepabo.com>
2024-03-07 21:29:02 +0900

    Loading.js追加

---
+++ src/components/Loading.js
@@ -0,0 +1,9 @@
+constLoading = () => {
+  return (
+    <div>
+      <h1>Loading...</h1>
+    </div>
+  );
+};
+
+export default Loading;
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,3 +1,5 @@
+import Loading from "../components/Loading";
+
 const Results = ({ countryData }) => {
   const { date, newConfirmed, totalConfirmed, newRecovered, totalRecovered } = countryData;
   return (
